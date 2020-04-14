848f1f43525743004797dcb02df631962372d2c5
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-14 13:37:49 +0300

    added search and searchRepo methods

--- src/app/searchbar/search-bar.component.ts
+++ src/app/searchbar/search-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'; 
 import { RequestService } from '../user-request/request.service';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,7 +10,36 @@ import { RequestService } from '../user-request/request.service';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  inputsearch=''
+  outputresult=[]
+  users:any=[]
+  reposearch=''
+  outputRepoSearch=[]
+  repos:any=[]
+
+  constructor(private requestservice:RequestService) { }
+
+    seaech(inputsearch:string){
+      let promise= new Promise((resolve,reject)=> {
+      this.requestservice.userInfo(this.inputsearch)
+      .toPromise().then((data=>{
+        this.outputresult=data['items'];
+        this.users=data;
+        console.log(data)
+      }))
+      })
+    }
+
+    searchRepo(reposearch:string){
+      let promise= new Promise((resolve,reject)=>{
+      this.requestservice.repoLink(this.reposearch)
+      .toPromise().then((data)=>{
+        this.outputRepoSearch=data['items'];
+        this.repos=data;
+        console.log(data)
+      })
+      })
+    }
 
   ngOnInit(): void {
   }
