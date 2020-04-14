61f45449b1449660769b1ed5208be51306029a42
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-14 14:00:30 +0300

    added reposLink and userInfo method

--- src/app/user-request/request.service.ts
+++ src/app/user-request/request.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import { HttpClient } from '@angular/common/http'
+ 
 @Injectable({
   providedIn: 'root'
 })
@@ -9,5 +10,15 @@ export class RequestService {
     username:string;
   private repo:string;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  reposLink(reposearch){
+
+    return this.http.get<JSON[]>('https://api.github.com/repos'+reposearch)
+  }
+
+  userInfo(inputsearch){
+
+    return this.http.get<JSON[]>('https://api.github.com/users/'+inputsearch)
+  }
 }
