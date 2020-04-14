3ba1c28fd688045921bd649d999ce1923624c276
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-14 13:52:59 +0300

    added apikey and username

--- src/app/user-request/request.service.ts
+++ src/app/user-request/request.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
+  apikey=environment.apikey;
+    username:string;
+  private repo:string;
 
   constructor() { }
 }
