b5edca01a61f9e6d77996baf05935dd1ec9ee24c
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-14 13:28:27 +0300

    imported RequestService and added providers to component

--- src/app/searchbar/search-bar.component.ts
+++ src/app/searchbar/search-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { RequestService } from '../user-request/request.service';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
+  providers:[RequestService],
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
