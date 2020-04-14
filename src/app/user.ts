1f3ff9147eabe224dc28c03cdedb0b8c6edc3c8e
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-14 14:27:01 +0300

    added constructor

--- src/app/user.ts
+++ src/app/user.ts
@@ -5,5 +5,13 @@ export class User {
   followers:string;
   repos=[];
 
-  
+  constructor(private onject:JSON){
+    this.name=Object['name'];
+    this.avatar=Object['avatar_url'];
+    this.followers=Object['followers_url'];
+    this.following=Object['following_url'];
+
+  }
+
+
 }
